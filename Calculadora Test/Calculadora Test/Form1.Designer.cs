06b7b0ce37a60e26ec948e19a6dcdaca800e557f
Davendanoc <57848662+Davendanoc@users.noreply.github.com>
2020-07-21 16:17:45 -0500

    Add files via upload

---
+++ Calculadora Test/Calculadora Test/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Calculadora_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selSum = new System.Windows.Forms.RadioButton();
+            this.selRes = new System.Windows.Forms.RadioButton();
+            this.selMul = new System.Windows.Forms.RadioButton();
+            this.selDi = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selSum
+            // 
+            this.selSum.AutoSize = true;
+            this.selSum.Location = new System.Drawing.Point(75, 84);
+            this.selSum.Name = "selSum";
+            this.selSum.Size = new System.Drawing.Size(52, 17);
+            this.selSum.TabIndex = 0;
+            this.selSum.TabStop = true;
+            this.selSum.Text = "Suma";
+            this.selSum.UseVisualStyleBackColor = true;
+            // 
+            // selRes
+            // 
+            this.selRes.AutoSize = true;
+            this.selRes.Location = new System.Drawing.Point(75, 121);
+            this.selRes.Name = "selRes";
+            this.selRes.Size = new System.Drawing.Size(53, 17);
+            this.selRes.TabIndex = 1;
+            this.selRes.TabStop = true;
+            this.selRes.Text = "Resta";
+            this.selRes.UseVisualStyleBackColor = true;
+            // 
+            // selMul
+            // 
+            this.selMul.AutoSize = true;
+            this.selMul.Location = new System.Drawing.Point(75, 155);
+            this.selMul.Name = "selMul";
+            this.selMul.Size = new System.Drawing.Size(89, 17);
+            this.selMul.TabIndex = 2;
+            this.selMul.TabStop = true;
+            this.selMul.Text = "Multiplicacion";
+            this.selMul.UseVisualStyleBackColor = true;
+            // 
+            // selDi
+            // 
+            this.selDi.AutoSize = true;
+            this.selDi.Location = new System.Drawing.Point(75, 187);
+            this.selDi.Name = "selDi";
+            this.selDi.Size = new System.Drawing.Size(62, 17);
+            this.selDi.TabIndex = 3;
+            this.selDi.TabStop = true;
+            this.selDi.Text = "Division";
+            this.selDi.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(289, 152);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(289, 197);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Seleccione la Operacion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(287, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Primer Numero";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Segundo Numero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(286, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Respuesta";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Operar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(283, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Ingrese los Datos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 301);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.selDi);
+            this.Controls.Add(this.selMul);
+            this.Controls.Add(this.selRes);
+            this.Controls.Add(this.selSum);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton selSum;
+        private System.Windows.Forms.RadioButton selRes;
+        private System.Windows.Forms.RadioButton selMul;
+        private System.Windows.Forms.RadioButton selDi;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Calculadora Test/Calculadora Test/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Test
+{
+    public partial class Form1 : Form
+    {
+
+        public Double x, y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string xt = textBox1.Text;
+            x = Convert.ToDouble(xt);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string yt = textBox2.Text;
+            y = Convert.ToDouble(yt);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double ans;
+
+
+            if (selSum.Checked == true)
+            {
+                ans = x + y;
+                textBox3.Text = ans.ToString("0.0000");
+            }
+
+            else if (selRes.Checked == true)
+            {
+                ans = x - y;
+                textBox3.Text = ans.ToString("0.0000");
+            }
+
+            else if (selMul.Checked == true)
+            {
+                ans = x * y;
+                textBox3.Text = ans.ToString("0.0000");
+            }
+
+            else if (selDi.Checked == true)
+            {
+                ans = x / y;
+                textBox3.Text = ans.ToString("0.0000");
+            }
+
+            else
+                textBox3.Text = "";
+        }
+    }
+}
+
